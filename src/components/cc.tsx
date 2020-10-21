c83c161c4b050ab0cd758969555af2938ec66417
xhe <xw897002528@gmail.com>
2020-10-21 11:20:53 +0800

    use local license svg, fix style
    Signed-off-by: xhe <xw897002528@gmail.com>

--- src/components/cc.tsx
+++ src/components/cc.tsx
@@ -4,10 +4,8 @@ export default defineComponent({
 	props: ['license'],
 	setup(props) {
 		return () => {
-			if (props.license)
-				return <a rel="license" href={`http://creativecommons.org/licenses/${props.license}/4.0/`}><img alt="license" src={`https://i.creativecommons.org/l/${props.license}/4.0/88x31.png`} /></a>
-			else
-				return ''
+			if (!props.license) return ''
+			return <a rel="license" href={`http://creativecommons.org/licenses/${props.license}/4.0/`}><img src={`/cc/${props.license}.svg`} /></a>
 		}
 	}
 })
