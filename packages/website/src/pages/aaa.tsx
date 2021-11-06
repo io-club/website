4802b9b36e67d81961b41047d1a6521f0e839cf1
trdthg <trdthg@outlook.com>
2021-11-06 15:37:47 +0800

    feat: add front demo
    Signed-off-by: trdthg <trdthg@outlook.com>

---
+++ packages/website/src/pages/aaa.tsx
@@ -0,0 +1,14 @@
+import '~/css/aaa.tsx'
+
+import { defineComponent} from 'vue'
+export default defineComponent({
+	setup() {
+		return () => {
+			return <div>
+				<h1>开始写你自己的html</h1>
+
+
+			</div>
+		}
+	}
+})
