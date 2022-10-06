7aace305bb2a3da79a454707020eb4dcc5de5976
xhe <xw897002528@gmail.com>
2022-10-07 02:14:13 +0800

    clean old css & use unocss
    Signed-off-by: xhe <xw897002528@gmail.com>

--- packages/website/app/types/attributify.d.ts
+++
@@ -1,6 +0,0 @@
-import type { UtilityNames, VariantNames } from 'windicss/types/jsx'
-
-export declare type ExtendedUtilityNames = UtilityNames | 'caret'
-export declare type ExtendedVariantNames = VariantNames
-
-export declare type AttributifyNames<Prefix extends string = ''> = `${Prefix}${ExtendedUtilityNames}` | `${Prefix}${ExtendedVariantNames}` | `${Prefix}${ExtendedVariantNames}:${ExtendedVariantNames}`
---
+++ packages/website/unocss.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, presetUno, presetAttributify, presetWebFonts, presetTypography, presetIcons } from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
+import { presetScalpel } from 'unocss-preset-scalpel'
+import { presetScrollbar } from 'unocss-preset-scrollbar'
+import presetDaisy from 'unocss-preset-daisy'
+import { presetHeroPatterns } from '@julr/unocss-preset-heropatterns'
+
+export default defineConfig({
+	presets: [
+		presetUno(),
+		presetAttributify({
+			prefix: 'w-',
+		}),
+		presetTypography(),
+		presetIcons(),
+		presetWebFonts(),
+		presetRemToPx,
+		presetScalpel(),
+		presetScrollbar(),
+		presetDaisy(),
+		presetHeroPatterns(),
+	],
+	shortcuts: [
+	],
+})
